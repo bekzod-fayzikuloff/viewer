84fa4d2679fd73b4d5368c642f15cc7f14f03221
berkf02 <63246331+berkf02@users.noreply.github.com>
2021-02-18 21:25:05 +0500

    'new_structure'

--- config/settings.py
+++ config/settings.py
@@ -6,5 +6,5 @@ def resource_path(relative):
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative)
     else:
-        return os.path.join(os.path.abspath("../first"), relative)
+        return os.path.join(os.path.abspath(""), relative)
 
---
+++ tests/run_tests.py
@@ -0,0 +1,7 @@
+import unittest
+from test_window_elements import SecondWindowElementTest, SliderElementTest, MyThreadElementTest
+from mainlyTest import ConfigTest, ApplicationsTest
+
+
+if __name__ == '__main__':
+    unittest.main()
